6f99552dbccbea21f5d995cc25c21ec9dbbb03fd
31418211 <31418211@087864-P33S105.macklabs.br>
2014-09-18 11:23:03 -0300

    exercicio restaurante concertado!!!!!

--- operacoes.php
+++ operacoes.php
@@ -1,53 +1,16 @@
 <?php
-
-    function mostra ($resultado){
-        echo "Resultado: ".$resultado;
+    function soma($x, $y){
+        return $x + $y;
     }
-
-    function soma ($x,$y){
-        $t=$x+$y;
-        return $t;
-    }
-
-    function subtração($x, $y){
-     $t = $x - $y;
-      return $t;
-      }
-    function multiplicação($x, $y){
-     $t = $x * $y;
-     return $t;
-     }
-     function divisão ($x, $y){
-     $t = $x / $y;
-     return $t;
-     }
-            
-    $n1 = 10;
-    $n2 = 5;
     
-   $resultado = soma($n1, $n2);
-
-   mostra($resultado);
-            
-   echo "<br><br>";
-            
-   $resultado = subtração($n1, $n2);
-
-   mostra($resultado);
-            
-   echo "<br><br>";
-            
-   $resultado = multiplicação($n1, $n2);
-
-   mostra($resultado);
-            
-   echo "<br><br>";
-            
-   $resultado = divisão($n1, $n2);
-
-   mostra($resultado);
-            
-            
-            echo "<br><br>";
-
-?>
+    function subtracao($x, $y){
+        return $x-$y;
+    }
+    
+    function multiplicacao($x, $y){
+        return $x*$y;
+    }    
+    function divisao($x, $y){
+        return $x/$y;
+    }
+    ?>
--- test.php
+++ test.php
@@ -1,9 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <head></head>
-    <body>
-        <?php include "operacoes.php"; 
-            
-        ?>
-    </body>
-</html>
+
+    <?php
+    include "operacoes.php";
+
+    $x=10;
+    $y=5;
+    $soma= soma($x, $y);
+    $subtracao = subtracao($x, $y);
+    $multiplicacao= multiplicacao($x, $y);
+    $divisao= divisao($x, $y);
+
+    echo "soma"+ $soma;
+    echo "</br>";
+    echo "subtração" +$subtracao;
+    echo "</br>";
+    echo "multiplicação"+ $multiplicacao;
+    echo "</br>";
+    echo "divisão"+ $divisao;
+    echo "</br>";
+
+    ?>
